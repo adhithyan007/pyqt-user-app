18b9f7258b4fc5eedf78e5495f35f440d929a64d
adhy <adhyaysu@gmail.com>
2022-04-11 18:39:15 +0530

    test end

--- usertest.py
+++ usertest.py
@@ -2,7 +2,8 @@ from tkinter import Label
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow
 import sys
-
+def clicked():
+    print("clicked")
 def demo():
     app= QApplication(sys.argv)
     win=QMainWindow()
@@ -12,6 +13,9 @@ def demo():
     label.setText("USER APP")
     label.move(100,100)
     
+    b1=QtWidgets.QPushButton(win)
+    b1.setText("Click me")
+    b1.clicked.connect(clicked)
     win.show()
     sys.exit(app.exec_())
     
