0ce661229679f2239a7a75c2f8143f7ff749feed
adhy <adhyaysu@gmail.com>
2022-04-11 18:49:09 +0530

    test 2

--- usertest.py
+++ usertest.py
@@ -10,7 +10,7 @@ def demo():
     win.setGeometry(300,300,300,300)
     win.setWindowTitle("USER DEMO APP")
     label=QtWidgets.QLabel(win)
-    label.setText("USER APP")
+    label.setText("USER demo APP")
     label.move(100,100)
     
     b1=QtWidgets.QPushButton(win)
